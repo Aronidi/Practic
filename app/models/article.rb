ac2de5b75e3210d8aa38855b9aac18bf31e7bab8
Deniom3 <den2om@mail.ru>
2018-07-01 18:28:01 +0300

    добавление и просмотр коментариев

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,4 +2,6 @@ class Article < ApplicationRecord
     validates :title, presence: true, length: { maximum: 25 }
     validates :adres, presence: true, length: { maximum: 75 }
     validates :price, presence: true,  numericality: { greater_than: 0 }
+    
+    has_many :comments
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,8 @@ Rails.application.routes.draw do
   get 'main/index'
   resources :articles #стать сайта
   root 'main#index'
+  resources :articles do
+    resources :comments
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
